d6843464b3e7a255da7797e4c6e2f54d5bd4b97e
Viren Mandaogane <70087991+viren1408@users.noreply.github.com>
2023-09-14 11:24:14 +0530

    Add files via upload
    The current code uses the CSV file of the pulsar candidates generated by SIBPF to check the variability at a particular pixel location. The SIBPF code gives the X and Y co ordinates of the sources . The sources with same X and Y co ordinates measured at different days are then written to a separate file which will contain the flux values of each observation as a separate column and the column names are edited appropriately. The code calculated the reduced ch-square statistics assuming no variability , this has be referenced from Bell et.al and Sett et.al. 
    Currently no crossmatching is done however I am trying to also figure that out if we can use pybdsf on specific pixel locations instead of the whole image. 
    The code has been tested for an image and its copy. If I could get any images for other days i could try testing it

---
+++ variable.py
@@ -0,0 +1,58 @@
+from os import error
+from turtle import color
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read the pulsar candidates DataFrames
+candidates_day_1 = pd.read_csv('Pulsar_candidates_G033.0-5.0.csv')
+candidates_day_2 = pd.read_csv('Pulsar_candidates_G033.0-5.0 _1.csv')
+candidates_day_3 = pd.read_csv('Pulsar_candidates_G033.0-5.0 _2.csv')
+candidates_day_4 = pd.read_csv('Pulsar_candidates_G033.0-5.0 _3.csv')
+
+# Merge the DataFrames based on 'Xposn' and 'Yposn' columns
+merged_df = candidates_day_1.merge(candidates_day_2, on=[' Xposn', ' Yposn'], suffixes=('_day1', '_day2'))
+merged_df = merged_df.merge(candidates_day_3, on=[' Xposn', ' Yposn'])
+merged_df = merged_df.merge(candidates_day_4, on=[' Xposn', ' Yposn'], suffixes=('_day3', '_day4'))
+
+# Initialize lists to store data
+flux_values = []
+error_values = []
+
+# Loop through candidates
+days = [1,2,3,4]
+for i in range(len(merged_df)):
+    # Extract flux and error values for each day
+    flux = [merged_df[ ' Total_flux_day1'][i],merged_df[ ' Total_flux_day2'][i],merged_df[ ' Total_flux_day3'][i],merged_df[ ' Total_flux_day4'][i]] 
+    error = [merged_df[ ' E_Total_flux_day1'][i],merged_df[ ' E_Total_flux_day2'][i],merged_df[ ' E_Total_flux_day3'][i],merged_df[ ' E_Total_flux_day4'][i]] 
+
+    # Calculate weighted mean (sbar_i) and weighted standard deviation
+    sbar_i = np.sum(np.array(flux) / np.array(error) ** 2) / np.sum(1 / np.array(error) ** 2)
+    weighted_std = np.sqrt(1 / np.sum(1 / np.array(error) ** 2))
+
+    #chi-squared 
+    chi_squared = np.sum(((np.array(flux) - sbar_i) / np.array(error)) ** 2)
+
+    #degrees of freedom
+    degrees_of_freedom = len(flux) - 1 
+
+    #reduced chi-squared (chi_sq_r)
+    chi_sq_r = chi_squared / degrees_of_freedom
+
+    # Append chi-squared and reduced chi-squared to lists
+    flux_values.append(sbar_i)
+    error_values.append(weighted_std)
+
+    print(f"Chi-squared for Candidate {i+1}: {chi_squared}")
+    print(f"Reduced Chi-squared for Candidate {i+1}: {chi_sq_r}")
+    plt.scatter(days,flux)
+    plt.errorbar(days,flux,yerr = error,alpha=0.7,ecolor='black',capsize=2,ls = 'none')
+    plt.plot(days,flux, linestyle='-', color='red')
+    plt.title(f"$\\chi^2$ for candidate {i+1}: {chi_squared}")
+    plt.grid()
+    plt.xlabel('Days')
+    plt.ylabel('Flux(Jy)')
+    plt.show()
+# Now you have lists containing the weighted mean (flux_values) and weighted standard deviation (error_values)
+# for each candidate, as well as the chi-squared and reduced chi-squared values for each candidate.
+# You can further analyze or plot these values as needed.
