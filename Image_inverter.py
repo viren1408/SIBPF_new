9460829177794459117e5a536ac4de0d305468de
Viren Mandaogane <70087991+viren1408@users.noreply.github.com>
2023-09-14 13:23:35 +0530

    Add files via upload
    This code detects artifacts in a radio image by inverting the pixel values and processing both the original and inverted images using the PyBDSF library to identify sources. It then calculates the fraction of sources detected in the inverted image compared to the original image, providing an estimate of false sources or artifacts resulting from noise fluctuations, thereby helping to assess the reliability of the detected sources in the radio image.

--- Image_inverter.py
+++ Image_inverter.py
@@ -1,5 +1,4 @@
 import numpy as np
-# Set up matplotlib
 import matplotlib.pyplot as plt
 from astropy.io import fits
 import astropy.units as u
@@ -8,8 +7,9 @@ from astropy.io import fits
 import bdsf
 import pandas as pd
 # Replace 'your_file.fits' with the actual file path to your FITS file
-region = 'G033.0-5.0'
-image_file = '/home/viren/work/DATA/'+str(region)+'.FITS'
+region = 'G024.0-5.6'
+dir = '/home/viren/work/DATA/inverter'
+image_file = str(dir)+'/'+str(region)+'.FITS'
 
 
 # Open the original FITS file
@@ -18,11 +18,11 @@ hdulist.info()
 # Access the image data
 image_data = hdulist[0].data[0,0]
 
-print(image_data)
+#print(image_data)
 
 
 # Invert the pixel values
-inverted_image_data = np.max(image_data) - image_data
+inverted_image_data =  -1.0*(image_data)
 
 # Create a new FITS header by copying the original header
 new_header = hdulist[0].header.copy()
@@ -31,22 +31,42 @@ new_header = hdulist[0].header.copy()
 new_hdulist = fits.HDUList([fits.PrimaryHDU(inverted_image_data, header=new_header)])
 
 # Save the new FITS file
-new_hdulist.writeto('/home/viren/work/DATA/'+str(region)+'_inverted_image.FITS', overwrite=True)
+new_hdulist.writeto(str(dir)+'/'+str(region)+'_inverted_image.FITS', overwrite=True)
 
-#Add Pybdsf block to get the artifact locations 
+#Pybdsf block to get the artifact locations 
 # Specify the file path and name
-#file_path = str(dir)+'/'+str(region)+'/'+str(region)+'.sav'
-file_path = '/home/viren/work/DATA/'+str(region)+'_inverted_image.sav' #create sav file
+file_path = str(dir)+'/'+str(region)+'_inverted_image.sav' #create sav file inverted
+file_path_1 = str(dir)+'/'+str(region)+'.sav' #create sav file
 # Open the file in write mode
-open(file_path, mode='w', newline='')
+open(file_path, mode='w', newline='') #inverted
+open(file_path_1, mode='w', newline='')
 # Load the input FITS file
-input_image = ('/home/viren/work/DATA/'+str(region)+'_inverted_image.FITS')
-save_file = '/home/viren/work/DATA/'+str(region)+'_inverted_image.sav' #sav file path 
-img = bdsf.process_image(save_file, filename=input_image, quiet=True)
-img.write_catalog(format='csv', catalog_type='srl',clobber = True)
-img.export_image(img_format = 'fits',img_type = 'gaus_model',clobber = True)
-data = pd.read_csv('/home/viren/work/DATA/'+str(region)+'_inverted_image.pybdsm.srl',index_col = None,skiprows=5) #change 1
+input_image = (str(dir)+'/'+str(region)+'_inverted_image.FITS') #inverted
+input_image_1 = (str(dir)+'/'+str(region)+'.FITS') #original
 
+save_file = str(dir)+'/'+str(region)+'_inverted_image.sav' #sav file path 
+save_file_1 = str(dir)+'/'+str(region)+'.sav' #sav file path original
+
+img = bdsf.process_image(save_file, filename=input_image, quiet=True) #inverted
+img_1 = bdsf.process_image(save_file_1, filename=input_image_1, quiet=True) #original
+
+img.write_catalog(format='csv', catalog_type='gaul',clobber = True) #inverted
+img_1.write_catalog(format='csv', catalog_type='gaul',clobber = True) #original
+
+img.export_image(img_format = 'fits',img_type = 'gaus_model',clobber = True) #inverted
+img_1.export_image(img_format = 'fits',img_type = 'gaus_model',clobber = True) #original
+
+data_inverted= pd.read_csv(str(dir)+'/' +str(region)+'_inverted_image.pybdsm.gaul',index_col = None,skiprows=5) # inverted
+data = pd.read_csv(str(dir)+'/' +str(region)+'.pybdsm.gaul',index_col = None,skiprows=5) 
+
+
+
+print(data_inverted)
+
+#Fraction of Sources 
+Inverted_fraction = len(data_inverted)/len(data)
+
+print(f"False detection fraction: {Inverted_fraction}")
 # Close both FITS files
 hdulist.close()
 new_hdulist.close()
